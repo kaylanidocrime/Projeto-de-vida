71bed9af8f49ee31d1c55c6b5376ccdc453ce4f1
kaylani <106694090+kaylanidocrime@users.noreply.github.com>
2024-02-21 14:40:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const botoes = document.querySelectA11(".botao");
 
+for(let i=0;i <botoes.legth;i++){
+  botoes[i].onclick = function(){
+
+    for(let j=0;j<botoes.legth;j++){
+      botoes[j].classList.remove("ativo");
+}
+
+    botoes[i].classList.add("ativo");
+  }
+}
